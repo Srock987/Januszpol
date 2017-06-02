fb2e24527e65417db3e39b903f46676087c1ca9d
Michal Perlak <michalperlak@windowslive.com>
2017-06-02 22:30:36 +0200

    Merge branch 'display_filters'

--- src/main/java/pl/edu/agh/eaiib/io/xp/controllers/workRecord/AllWorkRecordsController.java
+++ src/main/java/pl/edu/agh/eaiib/io/xp/controllers/workRecord/AllWorkRecordsController.java
@@ -2,18 +2,23 @@ package pl.edu.agh.eaiib.io.xp.controllers.workRecord;
 
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import pl.edu.agh.eaiib.io.xp.controllers.AbstractController;
 import pl.edu.agh.eaiib.io.xp.data.Database;
 import pl.edu.agh.eaiib.io.xp.model.WorkRecord;
 import pl.edu.agh.eaiib.io.xp.utils.TableButtonCallback;
 import pl.edu.agh.eaiib.io.xp.view.ScreenManager;
+import pl.edu.agh.eaiib.io.xp.view.filters.AndFilter;
+import pl.edu.agh.eaiib.io.xp.view.filters.FiltersFactory;
+import pl.edu.agh.eaiib.io.xp.view.filters.WorkRecordsViewFilter;
 
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class AllWorkRecordsController
     extends AbstractController {
@@ -35,6 +40,27 @@ public class AllWorkRecordsController
 
     @FXML
     private TableColumn<WorkRecord, String> deleteColumn;
+
+    @FXML
+    private TextField companyNameField;
+
+    @FXML
+    private TextField nrOfHoursField;
+
+    @FXML
+    private DatePicker beginDateControl;
+
+    @FXML
+    private DatePicker endDateControl;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private Button applyButton;
+
+    private List<WorkRecordsViewFilter> filters = FiltersFactory.getDefaultFilters();
+    private WorkRecordsViewFilter activeFilter = FiltersFactory.getEmptyFilter();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -70,5 +96,49 @@ public class AllWorkRecordsController
         deleteColumn.setCellFactory(deleteButtonCallback);
 
         workRecordsTableView.setItems(FXCollections.observableList(Database.getWorkRecords()));
+
+        filters = createFilters();
+
+        clearButton.setOnAction(e -> {
+            clearFiltersControls();
+            refreshFilters();
+        });
+
+        applyButton.setOnAction(e -> {
+            refreshFilters();
+        });
+    }
+
+    private List<WorkRecordsViewFilter> createFilters() {
+        List<WorkRecordsViewFilter> filters = new ArrayList<>();
+        filters.add(FiltersFactory.createCompanyNameFilter(companyNameField));
+        filters.add(FiltersFactory.createBeginDateFilter(beginDateControl));
+        filters.add(FiltersFactory.createEndDateFilter(endDateControl));
+        filters.add(FiltersFactory.createNumberOfHoursFilter(nrOfHoursField));
+
+        return filters;
+    }
+
+    private void clearFiltersControls() {
+        String empty = "";
+        companyNameField.setText(empty);
+        nrOfHoursField.setText(empty);
+        beginDateControl.getEditor().setText(empty);
+        endDateControl.getEditor().setText(empty);
+    }
+
+    private void refreshFilters() {
+        activeFilter = getActiveFilter();
+        refreshListView();
+    }
+
+    private WorkRecordsViewFilter getActiveFilter() {
+        List<WorkRecordsViewFilter> activeFilters = filters.stream().filter(WorkRecordsViewFilter::isActive).collect(Collectors.toList());
+        return new AndFilter(activeFilters);
+    }
+
+    private void refreshListView() {
+        List<WorkRecord> workRecords = Database.getWorkRecords().stream().filter(record -> activeFilter.accepts(record)).collect(Collectors.toList());
+        workRecordsTableView.setItems(FXCollections.observableList(workRecords));
     }
 }
